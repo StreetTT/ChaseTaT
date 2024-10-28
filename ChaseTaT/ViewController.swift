caa79559f437425a19e33dc72888ca7f82d007dc
Bow (Iris) <b.oyedepo@outlook.com>
2024-10-28 15:32:45 +0000

    New 7 Step Ladder
    Using draw and mask, recreated the ladder with UI buttons, changing the colour and size to match the original.

--- ChaseTaT/ViewController.swift
+++ ChaseTaT/ViewController.swift
@@ -9,36 +9,69 @@ import UIKit
 
 var amountToWin = [20000, 5000, 1000]
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var rung1: UIButton!
+    @IBOutlet weak var rung2: UIButton!
+    @IBOutlet weak var rung3: UIButton!
+    @IBOutlet weak var rung7: UIButton!
+    @IBOutlet weak var rung6: UIButton!
+    @IBOutlet weak var rung5: UIButton!
+    @IBOutlet weak var rung4: UIButton!
+    var rungs : [UIButton] = []
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
+    func applyLadderShape(to button: UIButton, ofset: CGFloat) -> CGFloat {
+        let slant = CGFloat(15)
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: ofset, y: 0))
+        path.addLine(to: CGPoint(x: button.bounds.width - ofset , y: 0))
+        path.addLine(to: CGPoint(x: button.bounds.width - ofset - slant, y: button.bounds.height))
+        path.addLine(to: CGPoint(x: ofset + slant , y: button.bounds.height))
+        path.close()
+        
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        button.layer.mask = mask
+        return CGFloat(ofset + slant)
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return CGFloat(100)
-        }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Set cell to the coresponding persons name in staff
-            
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        var content = UIListContentConfiguration.cell()
-        
-        content.text = if indexPath.row == 2 || indexPath.row == 3 || indexPath.row == 4 {
-            "£\(amountToWin[indexPath.row - 2])"
-        } else {
-            ""
-        }
-
-        cell.contentConfiguration = content
-        return cell
-            
-        }
-
+    
+    @objc func buttonClick(_ sender: UIButton){
+        print("clicked")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set the shape of the rungs
+        rungs = [rung1, rung2, rung3, rung4, rung5, rung6, rung7]
+        var x: CGFloat =  10
+        for rung in rungs {
+            x = applyLadderShape(to: rung, ofset: x)
+        }
+        
+        // Set each rung (Money, Colour)
+        for (index, rung) in rungs.enumerated() {
+            rung.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+            rung.addTarget(self, action: #selector(buttonClick(_:)), for: .touchUpInside)
+            if (2...4).contains(index) {
+                rung.setTitle("£\(amountToWin[index - 2])", for: .normal)
+                if index == 3 {
+                    rung.backgroundColor = UIColor(red: 5/255.0, green: 7/255.0, blue: 82/255.0, alpha: 1)
+                } else {
+                    rung.backgroundColor = UIColor(red: 10/255.0, green: 172/255.0, blue: 193/255.0, alpha: 1)
+                    rung.setTitleColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+                }
+            } else {
+                rung.setTitle("", for: .normal)
+                rung.backgroundColor = UIColor(red: 7/255.0, green: 177/255.0, blue: 158/255.0, alpha: 1)
+            }
+        }
+        
+        // Set Label
+        mainLabel.text = "Select an Offer: "
+        
+        // Register a button click
     }
 
 
